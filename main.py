8383a567dcf5b7c548d546f1a60b2d9584ff7b82
luna <luna@hackclub.app>
2025-02-25 11:21:53 +0800

    test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-
-args = sys.argv
+import subprocess
+from argparse import ArgumentParser
+from typing import List
 
 '''
 usage;
@@ -10,6 +10,21 @@ gpm [-u | --upstream] [remote] [branch]
 gpm "commit message" [remote] [branch]
 '''
 
-if args:
-    commitmsg = args[0]
-    args.pop(0)
+parser = ArgumentParser(
+    prog="automatic git comitter",
+    description="helps to stages all changes, commits, and pushes, with args",
+    epilog="made by luna :3"
+)
+
+parser.add_argument("message")
+parser.add_argument("-u", "--upstream", required=False, default="origin/main")
+parser.add_argument("-f", "--force", required=False, action='store_true')
+parser.add_argument("-q", "--quiet", required=False, action='store_true')
+parser.add_argument("-v", "--verbose", required=False, action='store_true')
+parser.add_argument("-nomsg", "-n", "--allow-empty", "--no-message", required=False, action='store_true')
+
+args = parser.parse_args()
+
+gac: List[str] = ['git', 'add']
+gcc: List[str] = ['git', 'commit']
+gpc: List[str] = ['git', 'push']
