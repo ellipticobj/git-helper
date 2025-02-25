93348805de469a996cc26bc35c9f7fc96bfbae07
luna <luna@hackclub.app>
2025-02-25 12:08:20 +0800

    main

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# git-helper
+# git helper
 im tired of doing git add . git commit -m "" git push all the time and im too lazy to make a macro
+
+## arguments:
--- main.py
+++ main.py
@@ -18,21 +18,21 @@ def run(args: List[str], verbose: bool, dry: bool=False) -> None:
 
 def main() -> None:
     parser = ArgumentParser(
-        prog="automatic git comitter",
+        prog="git helper",
         description="helps to stages all changes, commits, and pushes, with args",
         epilog="made by luna :3"
     )
 
-    parser.add_argument("message", required=False, help="commit message")
+    parser.add_argument("message", nargs='?', help="commit message")
     parser.add_argument("-u", "--upstream", required=False, help="upstream branch to push to")
-    parser.add_argument("-d", "--dry-run", dest="dry", required=False, action="store_true", help="prints out the commands that will be run without actually running them")
+    parser.add_argument("-d", "--dry-run", "--dry", dest="dry", required=False, action="store_true", help="prints out the commands that will be run without actually running them")
     parser.add_argument("-f", "--force", required=False, action='store_true', help="force push")
     parser.add_argument("-q", "--quiet", required=False, action='store_true', help="quiet")
     parser.add_argument("-v", "--verbose", required=False, action='store_true', help="verbose")
     parser.add_argument("-n", "--allow-empty-message", "--no-message", dest="nomsg", required=False, action='store_true', help="allows empty commit message")
     parser.add_argument("--allow-empty", dest="empty", required=False, action='store_true', help="allows empty commit")
     parser.add_argument("--pull", action="store_true", help="runs git pull before pushing before pushing")
-    parser.add_argument("--pull-no-rebase", action="store_true", help="runs git pull --no-rebase before pushing")
+    parser.add_argument("--pull-no-rebase", dest="norebase", action="store_true", help="runs git pull --no-rebase before pushing")
     parser.add_argument("--update-submodules", action="store_true", help="update submodules recursively")
 
     args = parser.parse_args()
@@ -47,6 +47,7 @@ def main() -> None:
     if args.update_submodules:
         run(['git', 'submodule', 'update', '--init', '--recursive'], verbose, args.dry)
 
+    gpullc: List[str] = ['git', 'pull']
     gac: List[str] = ['git', 'add']
     gcc: List[str] = ['git', 'commit']
     gpc: List[str] = ['git', 'push']
@@ -65,12 +66,11 @@ def main() -> None:
         gpc.append('--force')
 
     if args.quiet:
-        gpc.append('--quiet')
-        gcc.append('--quiet')
+        for i in [gpullc, gpc, gcc]:
+            i.append('--quiet')
     elif verbose:
-        gpc.append('--verbose')
-        gcc.append('--verbose')
-        gac.append('--verbose')
+        for i in [gpullc, gpc, gcc, gac]:
+            i.append('--verbose')
 
     if args.nomsg:
         gcc.append('--allow-empty-message')
@@ -79,12 +79,15 @@ def main() -> None:
         gcc.append('--allow-empty')
 
     if args.pull:
-        run(['git', 'pull'], verbose, args.dry)
+        if args.norebase:
+            gpullc.append('--no-rebase')
+        run(gpullc, verbose, args.dry)
 
     for i in [gac, gcc, gpc]:
         run(i, verbose, args.dry)
 
-    print("done")
+    if not args.dry:
+        print("done")
 
 if __name__ == "__main__":
     main()
