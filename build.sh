c5a0e5f9ebe5ff4b5b18f4164489f6553caa4b47
luna <luna@hackclub.app>
2025-02-28 00:30:17 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -m PyInstaller --onefile main.py -n meow--hidden-import=colorama --hidden-import=tqdm
+python -m PyInstaller --onefile main.py -n meow --hidden-import=colorama --hidden-import=tqdm
