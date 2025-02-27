b327d88d4c4ba28608e925483227ad65e902ebcf
luna <luna@hackclub.app>
2025-02-28 00:19:10 +0800

    updated version

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from tqdm import tqdm # type: ignore
 # Initialize colorama
 init(autoreset=True)
 
-VERSION = "0.2.0"
+VERSION = "0.2.1-preview"
 
 def success(message: str) -> None:
     '''print success message'''
